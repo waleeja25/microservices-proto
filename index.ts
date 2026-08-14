5df1966798372effa33e1cc928c46d885b4ce712
Waleeja Ali Rana <waleeja.ali25@gmail.com>
2026-08-15 01:53:25 +0500

    feat: update index file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * as OrderProto from './generated/order';
 export * as UserProto from './generated/user';
 export * as CatalogProto from './generated/catalog';
+export * as GoogleProtobuf from './generated/google/protobuf/empty';
 
